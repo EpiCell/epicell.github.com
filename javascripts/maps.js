c1c0da1364e3b06b4492578bffc165811f392f76
Ryan Balfanz <ryan@ryanbalfanz.net>
2011-11-12 14:13:56 -0800

    Updated gh-pages
    Signed-off-by: Ryan Balfanz <ryan@ryanbalfanz.net>

---
+++ javascripts/maps.js
@@ -0,0 +1,35 @@
+$(function() {
+
+	//Basic
+	var cartodbMapOptions = {
+		zoom: 5,
+		center: new google.maps.LatLng( 22.7248057566452, -106.9967118782795 ),
+		disableDefaultUI: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	// Init the map
+	var carto_map = new google.maps.Map(document.getElementById("malaria-map"),cartodbMapOptions);
+
+	map_style = [ { stylers: [ { saturation: -65 }, { gamma: 1.52 } ] }, { featureType: "administrative", stylers: [ { saturation: -95 },{ gamma: 2.26 } ] },
+	{ featureType: "water", elementType: "labels", stylers: [ { visibility: "off" } ] },{ featureType: "administrative.locality", stylers: [ { visibility: 'off' } ] },
+	{ featureType: "road", stylers: [ { visibility: "simplified" }, { saturation: -99 }, { gamma: 2.22 } ] }, { featureType: "poi", elementType: "labels", stylers: [ { visibility: "off" } ] },
+	{ featureType: "road.arterial", stylers: [ { visibility: 'off' } ] }, { featureType: "road.local", elementType: "labels", stylers: [ { visibility: 'off' } ] },
+	{ featureType: "transit", stylers: [ { visibility: 'off' } ] }, { featureType: "road", elementType: "labels", stylers: [ { visibility: 'off' } ] },
+	{ featureType: "poi", stylers: [ { saturation: -55 } ] } ];
+
+	carto_map.setOptions({styles: map_style});
+
+	// Add the cartodb tiles
+	var cartodb_layer = {
+		getTileUrl: function(coord, zoom) {
+			return "https://examples.cartodb.com/tiles/monarch/" + zoom + "/" + coord.x + "/" + coord.y + ".png" +
+			"?sql=SELECT monarch.cartodb_id, monarch.the_geom_webmercator FROM monarch, countries WHERE ST_Intersects(monarch.the_geom,countries.the_geom) AND countries.name = 'Mexico'";
+		},
+		tileSize: new google.maps.Size(256, 256)
+	};
+
+	var cartodb_imagemaptype = new google.maps.ImageMapType(cartodb_layer);
+
+	carto_map.overlayMapTypes.insertAt(0, cartodb_imagemaptype);
+});
